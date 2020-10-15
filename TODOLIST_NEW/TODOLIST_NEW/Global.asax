39372eaab40218cb923eba13982da6d8491e3ad4
sadanan <Sadanun.1997@gmail.com>
2020-10-15 23:24:26 +0700

    upfile

---
+++ TODOLIST_NEW/TODOLIST_NEW/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TODOLIST_NEW.MvcApplication" Language="C#" %>
---
+++ TODOLIST_NEW/TODOLIST_NEW/topicController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TODOLIST_NEW
+{
+    public class topicController : Controller
+    {
+        // GET: topic
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
